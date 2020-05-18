204e5053209d605abb8349e92e1cac5d25a993ba
Mark <sipizork@gmail.com>
2020-05-18 23:14:44 +0200

    creating a cah game

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,82 @@
+import React from 'react'
+
+import './homepage.styles.scss'
+
+const HomePage = () => (
+  <div className="board-container">
+    <div className="board">
+      <div className="player-container">
+        <img src="https://siposmark.com/assets/profil.png" alt="profil" className="profile-pic" />
+        <div className="points">
+          <span>1</span>
+        </div>
+      </div>
+      <div className="player-container">
+        <img src="https://siposmark.com/assets/profil.png" alt="profil" className="profile-pic" />
+          <div className="points">
+            <span>2</span>
+        </div>
+      </div>
+      <div className="player-container">
+        <img src="https://siposmark.com/assets/profil.png" alt="profil" className="profile-pic" />
+          <div className="points">
+            <span>3</span>
+        </div>
+      </div>
+      <div className="player-container">
+        <img src="https://siposmark.com/assets/profil.png" alt="profil" className="profile-pic" />
+          <div className="points">
+            <span>4</span>
+        </div>
+      </div>
+      <div className="player-container">
+        <img src="https://siposmark.com/assets/profil.png" alt="profil" className="profile-pic" />
+          <div className="points">
+            <span>5</span>
+        </div>
+      </div>
+      <div className="player-container">
+        <img src="https://siposmark.com/assets/profil.png" alt="profil" className="profile-pic" />
+          <div className="points">
+            <span>6</span>
+        </div>
+      </div>
+      <div className="black-cards"><p>Cards Againts Humanity</p></div>
+      <div className="revealed-black-card"><p>Tegnap ____ volt nálam. Nagyon élveztem.</p></div>
+    </div>
+    <div className="player-cards">
+      <div className="card">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ac interdum ipsum. Donec sed velit euismod</p>
+      </div>
+      <div className="card">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ac interdum ipsum. Donec sed velit euismod</p>
+      </div>
+      <div className="card">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ac interdum ipsum. Donec sed velit euismod</p>
+      </div>
+      <div className="card">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ac interdum ipsum. Donec sed velit euismod</p>
+      </div>
+      <div className="card">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ac interdum ipsum. Donec sed velit euismod</p>
+      </div>
+      <div className="card">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ac interdum ipsum. Donec sed velit euismod</p>
+      </div>
+      <div className="card">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ac interdum ipsum. Donec sed velit euismod</p>
+      </div>
+      <div className="card">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ac interdum ipsum. Donec sed velit euismod</p>
+      </div>
+      <div className="card">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ac interdum ipsum. Donec sed velit euismod</p>
+      </div>
+      <div className="card">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ac interdum ipsum. Donec sed velit euismod</p>
+      </div>
+    </div>
+  </div>
+)
+
+export default HomePage
